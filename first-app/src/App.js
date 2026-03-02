d962e05494902027370d6e1c19cc8d8461cae4b4
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-03-02 08:36:27 -0300

    fazendo o exemplo 4

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exemplo04 from './jsx_examples/exemplo-04-listas';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Exemplo04 />
   );
 }
 
